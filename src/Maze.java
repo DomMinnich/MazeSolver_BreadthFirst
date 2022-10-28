5451665297dba78d3c68109d1e2784f233eb4df6
Dominic Minnich <dominicminnich@gmail.com>
2022-10-28 08:52:08 -0400

    comment

---
+++ src/Maze.java
@@ -0,0 +1,35 @@
+/*
+ * 1
+ * public class Maze (Scanner input)
+ * The maze has '|' as walls and are not passable
+ * The maze has '_' which can be passed through going west or east
+ * The maze has ' ' which can be passed through going north or south or east or
+ * west
+ * The starting position is assumed to be in the upper left corner, while the
+ * finishing position is assumed to be in the lower right corner.
+ * Internally the maze should be represented as a two-dimensional array of Cells
+ */
+
+/*
+ * 2
+ * Method public findPath()
+ * This method will find the path from the start to the end.
+ * If path exists the method records the path and marks the path with 'X'
+ * The algorithm operates according to the following pseudo-code: Visit the
+ * starting Location. Add this Location to the Set. Enqueue the Location in the
+ * Queue.
+ */
+
+/*
+ * 3
+ * Method public hasPath()
+ * This method returns true if a path exists, false otherwise based on the
+ * solve() method
+ */
+
+/*
+ * 4
+ * public printMaze()
+ * This method prints the maze to the console
+ * 
+ */
