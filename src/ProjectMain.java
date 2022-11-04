3079d9291ddff1f5988e27c953fbf3c70f0ec6b3
Dominic Minnich <dominicminnich@gmail.com>
2022-11-03 21:48:38 -0400

    space removed

--- src/ProjectMain.java
+++ src/ProjectMain.java
@@ -5,6 +5,5 @@ public class ProjectMain {
     public static void main(String[] args) throws Exception {
         System.out.println("Start!");
         Application.launch(MazeGUI.class, args);
-
     }
 }
